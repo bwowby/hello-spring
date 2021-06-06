8f37ad06d2fcd2bb9c1eb5163cba6889ce424794
bwowby <pring120@gmail.com>
2021-06-06 21:45:37 +0900

    initial commit

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,22 @@
+package hello.hellospring.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+    //hello라고 url로 들어오면 여기로 연결해 줌
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data","hellooo!!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model){
+        model.addAttribute("name",name);
+        return "hello-template";
+    }
+}
