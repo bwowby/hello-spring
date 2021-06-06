ff6e55f89c49ff960f9447567c3171af84fbf926
bwowby <pring120@gmail.com>
2021-06-06 22:04:50 +0900

    feat : mvc-api(ResponseBody)

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -18,5 +19,32 @@ public class HelloController {
     public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model){
         model.addAttribute("name",name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody // html 바디에 데이터를 직접 넣어주겠다 -> 뷰 없이 그냥 이 문자열 넘김
+    public String helloString(@RequestParam("name") String name){
+        return "hello "+ name;
+    }
+
+    @GetMapping("hello-api")
+    //viewResolver 대신에 HttpMessageConverter가 동작(jackson이 json으로 변환)
+    @ResponseBody
+    public Hello HelloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello{
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
